156eeed066b3d54f153be7d0154b066a67a1b9a6
ARJUN KUMAR <unabletofindmail@gmail.com>
2026-06-15 11:51:01 +0530

    Add: longest-substring-without-duplicates - submission-2

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-2.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    lengthOfLongestSubstring(s) {
+
+        let maxLen = 0;
+        let set = new Set();
+        let left = 0;
+
+        for (let right = 0; right <s.length;right++){
+
+            while(set.has(s[right])){
+                set.delete(s[left]);
+                left++;
+            }
+
+            set.add(s[right]);
+            maxLen = Math.max(maxLen, set.size);
+            
+        }
+        return maxLen;
+    }
+}
